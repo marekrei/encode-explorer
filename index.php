28aeae8dd8810829f32dafe302a34291c68cef21
Marek Rei <marek@siineiolekala.net>
2011-09-09 13:25:32 +0300

    Added localisation of "Log out" and the Finnish translation. Thanks Miikka!

--- index.php
+++ index.php
@@ -449,7 +449,8 @@ $_TRANSLATIONS["en"] = array(
 	"username" => "Username",
 	"log_in" => "Log in",
 	"upload_type_not_allowed" => "This file type is not allowed for uploading.",
-	"del" => "Del" // short for Delete
+	"del" => "Del", // short for Delete
+	"log_out" => "Log out"
 );
 
 // Estonian
@@ -470,6 +471,38 @@ $_TRANSLATIONS["et"] = array(
 	"unable_to_read_dir" => "Unable to read directory",
 	"location" => "Asukoht",
 	"root" => "Peakaust"
+);
+
+// Finnish
+$_TRANSLATIONS["fi"] = array(
+	"file_name" => "Tiedoston nimi",
+	"size" => "Koko",
+	"last_changed" => "Muokattu",
+	"total_used_space" => "Yhteenlaskettu koko",
+	"free_space" => "Vapaa tila",
+	"password" => "Salasana",
+	"upload" => "Lisää tiedosto",
+	"failed_upload" => "Tiedoston lisäys epäonnistui!",
+	"failed_move" => "Tiedoston siirto kansioon epäonnistui!",
+	"wrong_password" => "Väärä salasana",
+	"make_directory" => "Uusi kansio",
+	"new_dir_failed" => "Uuden kansion luonti epäonnistui!",
+	"chmod_dir_failed" => "Kansion käyttäjäoikeuksien muuttaminen epäonnistui!",
+	"unable_to_read_dir" => "Kansion sisältöä ei voi lukea.",
+	"location" => "Paikka",
+	"root" => "Juurihakemisto",
+	"log_file_permission_error" => "Ohjelman ei ole sallittu kirjoittaa lokiin.",
+	"upload_not_allowed" => "Ohjelman asetukset eivät salli tiedoston lisäämistä tähän kansioon.",
+	"upload_dir_not_writable" => "Kansioon tallentaminen epäonnistui.",
+	"mobile_version" => "Mobiilinäkymä",
+	"standard_version" => "Tavallinen näkymä",
+	"page_load_time" => "Sivu ladattu %.2f ms:ssa",
+	"wrong_pass" => "Väärä käyttäjätunnus tai salasana",
+	"username" => "Käyttäjätunnus",
+	"log_in" => "Kirjaudu sisään",
+	"log_out" => "Kirjaudu ulos",
+	"upload_type_not_allowed" => "Tämän tiedostotyypin lisääminen on estetty.",
+	"del" => "Poista"
 );
 
 // French
@@ -2976,7 +3009,7 @@ if(GateKeeper::isAccessAllowed() && $this->location->uploadAllowed() && (GateKee
 <div id="info">
 <?php
 if(GateKeeper::isUserLoggedIn())
-	print "<a href=\"".$this->makeLink(false, true, null, null, null, "")."\">Log out</a> | ";
+	print "<a href=\"".$this->makeLink(false, true, null, null, null, "")."\">".$this->getString("log_out")."</a> | ";
 
 if(EncodeExplorer::getConfig("mobile_enabled") == true)
 {

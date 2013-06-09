1d1c63ee0fa33acf24afccdd8d020a165776a086
kris <kris@xoofoo.org>
2013-06-09 19:31:51 +0200

    update french translation

--- index.php
+++ index.php
@@ -510,8 +510,8 @@ $_TRANSLATIONS["fi"] = array(
 $_TRANSLATIONS["fr"] = array(
 	"file_name" => "Nom de fichier",
 	"size" => "Taille",
-	"last_changed" => "Ajout&eacute;",
-	"total_used_space" => "Espace total utilis&eacute;",
+	"last_changed" => "Ajouté",
+	"total_used_space" => "Espace total utilisé",
 	"free_space" => "Espace libre",
 	"password" => "Mot de passe",
 	"upload" => "Envoyer un fichier",
@@ -519,11 +519,23 @@ $_TRANSLATIONS["fr"] = array(
 	"failed_move" => "Erreur lors du changement de dossier",
 	"wrong_password" => "Mauvais mot de passe",
 	"make_directory" => "Nouveau dossier",
-	"new_dir_failed" => "Erreur lors de la cr&eacute;ation du dossier",
+	"new_dir_failed" => "Erreur lors de la création du dossier",
 	"chmod_dir_failed" => "Impossible de changer les permissions du dossier",
 	"unable_to_read_dir" => "Impossible de lire le dossier",
 	"location" => "Localisation",
-	"root" => "Racine"
+	"root" => "Racine",
+	"log_file_permission_error" => "Le script ne possède pas les permissions pour écrire un fichier d'enregistrement.",
+	"upload_not_allowed" => "La configuration actuelle ne permet de déposer un fichier dans ce dossier.",
+	"upload_dir_not_writable" => "Ce dossier ne possède pas les permissions en écriture.",
+	"mobile_version" => "Vue mobile",
+	"standard_version" => "Vue standard",
+	"page_load_time" => "Page chargée en %.2f ms",
+	"wrong_pass" => "Identifiant ou mot de passe incorrect",
+	"username" => "Identifiant",
+	"log_in" => "Connexion",
+	"upload_type_not_allowed" => "Ce format de fichier n'est pas autorisé.",
+	"del" => "Eff.", 
+	"log_out" => "Déconnexion"
 );
 
 // German

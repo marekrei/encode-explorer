ef3b373eb5f6ab5df2b336d0240717425c280833
NewEraCracker <neweracracker@gmail.com>
2015-04-02 20:29:50 +0100

    Add Portuguese (Portugal) translation

--- index.php
+++ index.php
@@ -708,6 +708,38 @@ $_TRANSLATIONS["pt_BR"] = array(
 	"upload_type_not_allowed" => "Não é permitido envio de arquivos deste tipo."
 );
 
+// Portuguese (Portugal)
+$_TRANSLATIONS["pt_PT"] = array(
+	"file_name" => "Nome do ficheiro",
+	"size" => "Tamanho",
+	"last_changed" => "Modificado em",
+	"total_used_space" => "Total de espaço utilizado",
+	"free_space" => "Espaço livre",
+	"password" => "Palavra-passe",
+	"upload" => "Enviar",
+	"failed_upload" => "Falha ao enviar o ficheiro!",
+	"failed_move" => "Falha ao mover o ficheiro para a pasta correcta!",
+	"wrong_password" => "Palavra-passe errada",
+	"make_directory" => "Nova pasta",
+	"new_dir_failed" => "Falha ao criar pasta",
+	"chmod_dir_failed" => "Falha ao mudar os privilégios da pasta",
+	"unable_to_read_dir" => "Não foi possível ler a pasta",
+	"location" => "Localização",
+	"root" => "Raíz",
+	"log_file_permission_error" => "O script não tem permissão para escrever o ficheiro de log.",
+	"upload_not_allowed" => "A configuração do script não permite envios nesta pasta.",
+	"upload_dir_not_writable" => "Não há permissão para escrita nesta pasta.",
+	"mobile_version" => "Versão Móvel",
+	"standard_version" => "Versão Padrão",
+	"page_load_time" => "Página carregada em %.2f ms",
+	"wrong_pass" => "Nome de utilizador ou palavra-passe errados",
+	"username" => "Nome de utilizador",
+	"log_in" => "Entrar",
+	"upload_type_not_allowed" => "Não é permitido o envio de ficheiros deste tipo.",
+	"del" => "Apagar",
+	"log_out" => "Sair"
+);
+
 // Romanian
 $_TRANSLATIONS["ro"] = array(
 	"file_name" => "Nume fisier",

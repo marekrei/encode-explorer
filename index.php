76ea2d6c4e2e14b229ca9a04e7de443214e79671
Marek Rei <marek.rei@gmail.com>
2015-07-02 00:00:39 +0100

    Updated Italian

--- index.php
+++ index.php
@@ -587,9 +587,21 @@ $_TRANSLATIONS["it"] = array(
 	"make_directory" => "Nuova cartella",
 	"new_dir_failed" => "Creazione cartella fallita!",
 	"chmod_dir_failed" => "Modifica dei permessi della cartella fallita!",
-	"unable_to_read_dir" => "Unable to read directory",
-	"location" => "Location",
-	"root" => "Root"
+	"unable_to_read_dir" => "Non abilitato a leggere la cartella",
+	"location" => "Posizione",
+	"root" => "Indice",
+	"log_file_permission_error" => "Lo script non ha i permessi per scrivere il file di log.",
+	"upload_not_allowed" => "La configurazione dello script non permette l'upload in questa cartella.",
+	"upload_dir_not_writable" => "Questa cartella non ha i permessi di scrittura.",
+	"mobile_version" => "Visualizzazione Mobile",
+	"standard_version" => "Visualizzazione Standard",
+	"page_load_time" => "Page aperta in %.2f min",
+	"wrong_pass" => "Username o password errati",
+	"username" => "Username",
+	"log_in" => "Log in",
+	"upload_type_not_allowed" => "Questo formato di file non è abilitato per l'upload.",
+	"del" => "Cancella", // short for Delete
+	"log_out" => "Esci"
 );
 
 // Norwegian

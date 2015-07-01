7766ab214daf7ecc68e37ca15c5c2b9ba421654f
Marek Rei <marek.rei@gmail.com>
2015-07-01 23:59:43 +0100

    Added Dutch

--- index.php
+++ index.php
@@ -357,9 +357,21 @@ $_TRANSLATIONS["nl"] = array(
 	"make_directory" => "Nieuwe folder",
 	"new_dir_failed" => "Fout bij aanmaken folder!",
 	"chmod_dir_failed" => "Rechten konden niet gewijzigd worden!",
-	"unable_to_read_dir" => "Unable to read directory",
-	"location" => "Location",
-	"root" => "Root"
+	"unable_to_read_dir" => "Niet mogelijk om directorie te lezen",
+	"location" => "Locatie",
+	"root" => "Root",
+	"log_file_permission_error" => "Script heeft geen toegang tot het logbestand.",
+	"upload_not_allowed" => "Uploaden van bestanden is niet toegestaan.",
+	"upload_dir_not_writable" => "Het is niet toegestaan in deze directorie bestanden te plaatsen.",
+	"mobile_version" => "Mobiele weergave",
+	"standard_version" => "Standaard weergave",
+	"page_load_time" => "Pagina geladen in %.2f ms",
+	"wrong_pass" => "Foutieve gebruikersnaam of wachtwoord",
+	"username" => "Gebruikersnaam",
+	"log_in" => "Inloggen",
+	"upload_type_not_allowed" => "Dit type bestand is niet toegestaan.",
+	"del" => "Verwijder", // short for Delete
+	"log_out" => "Uitloggen"
 );
 
 // English

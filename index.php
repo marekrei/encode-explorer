74181b7d3659a565d7534a2bb37573c24716bfff
Marek Rei <marek.rei@gmail.com>
2015-07-02 00:01:53 +0100

    Added Esperanto

--- index.php
+++ index.php
@@ -850,6 +850,39 @@ $_TRANSLATIONS["tr"] = array(
 	"root" => "Root"
 );
 
+
+// Esperanto
+$_TRANSLATIONS["eo"] = array(
+	"file_name" => "Dosiernomo",
+	"size" => "Grando",
+	"last_changed" => "Lasta ŝanĝo",
+	"total_used_space" => "Uzata spaco",
+	"free_space" => "Disponebla spaco",
+	"password" => "pasvorto",
+	"upload" => "Alŝuto",
+	"failed_upload" => "Alŝuto malsukcesis!",
+	"failed_move" => "Movo de la dosiero malsukcesis!",
+	"wrong_password" => "Malĝusta pasvorto",
+	"make_directory" => "Nova dosierujo",
+	"new_dir_failed" => "Kreado de dosierujo malsukcesis",
+	"chmod_dir_failed" => "Ŝanĝo de dosierujaj rajtoj malsukcesis",
+	"unable_to_read_dir" => "Dosierujo ne estas legebla",
+	"location" => "Loko",
+	"root" => "Radiko",
+	"log_file_permission_error" => "La skripto ne rajtas skribi la protokolan dosieron.",
+	"upload_not_allowed" => "La skripto malpermesas alŝuti en ĉi tiun dosierujon.",
+	"upload_dir_not_writable" => "Ĉi tiu dosierujo ne rajtigas skribadon.",
+	"mobile_version" => "Vido por mobilaj iloj",
+	"standard_version" => "Defaŭlta vido",
+	"page_load_time" => "Paĝo ŝarĝita en %.2f ms",
+	"wrong_pass" => "Malĝusta salutnomo aŭ pasvorto",
+	"username" => "Salutnomo",
+	"log_in" => "Ensaluto",
+	"upload_type_not_allowed" => "Alŝuto estas malpermesita por ĉi tiu dosiertipo.",
+	"del" => "For", // short for Delete
+	"log_out" => "Adiaŭo"
+);
+
 /***************************************************************************/
 /*   CSS FOR TWEAKING THE DESIGN                                           */
 /***************************************************************************/

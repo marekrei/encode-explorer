4a238fefc29798bb9d826b2818adc4f737f0e02e
trujulu <trujulu@gmail.com>
2015-12-27 12:25:28 +0100

    Updated Spanish translation to match current default English translation

--- index.php
+++ index.php
@@ -826,22 +826,34 @@ $_TRANSLATIONS["sk"] = array(
 
 // Spanish
 $_TRANSLATIONS["es"] = array(
-	"file_name" => "Nombre de archivo",
-	"size" => "Medida",
-	"last_changed" => "Ultima modificación",
-	"total_used_space" => "Total espacio usado",
+	"file_name" => "Nombre del archivo",
+	"size" => "Tamaño",
+	"last_changed" => "Último cambio",
+	"total_used_space" => "Espacio total usado",
 	"free_space" => "Espacio libre",
 	"password" => "Contraseña",
 	"upload" => "Subir el archivo",
-	"failed_upload" => "Error al subir el archivo!",
-	"failed_move" => "Error al mover el archivo al directorio seleccionado!",
+	"failed_upload" => "¡Error al subir el archivo!",
+	"failed_move" => "¡Error al mover el archivo al directorio seleccionado!",
 	"wrong_password" => "Contraseña incorrecta",
 	"make_directory" => "Crear directorio",
 	"new_dir_failed" => "Error al crear el directorio",
-	"chmod_dir_failed" => "Error al cambiar los derechos de directorio",
+	"chmod_dir_failed" => "Error al cambiar los permisos del directorio",
 	"unable_to_read_dir" => "No es posible leer el directorio",
 	"location" => "Localización",
-	"root" => "Raíz"
+	"root" => "Raíz",
+	"log_file_permission_error" => "El script no tiene permisos para escribir en el archivo de registro.",
+	"upload_not_allowed" => "La configuración del script no permite subir archivos en este directorio.",
+	"upload_dir_not_writable" => "Este directorio no tiene permisos de escritura.",
+	"mobile_version" => "Vista móvil",
+	"standard_version" => "Vista estándar",
+	"page_load_time" => "Página cargada en %.2f ms",
+	"wrong_pass" => "Nombre de usuario o contraseña incorrectos",
+	"username" => "Usuario",
+	"log_in" => "Iniciar sesión",
+	"upload_type_not_allowed" => "Este tipo de archivo no está permitido para la subida.",
+	"del" => "Eliminar",
+	"log_out" => "Cerrar sesión"
 );
 
 // Swedish

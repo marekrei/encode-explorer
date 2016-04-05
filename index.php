29c5e457f447f044df87b3e46a8cedfba1c85d2e
NewEraCracker <neweracracker@gmail.com>
2016-04-05 10:37:14 +0100

    Better indentation

--- index.php
+++ index.php
@@ -2823,18 +2823,18 @@ class EncodeExplorer
 	{
 		// Only perform charset convertion if output configured to be in UTF-8 and input isn't already UTF-8
 		if( EncodeExplorer::getConfig('charset') == 'UTF-8' &&
-			EncodeExplorer::getConfig('os_charset') != 'UTF-8' &&
-			// From http://w3.org/International/questions/qa-forms-utf-8.html
-			!preg_match('%^(?:
-			  [\x09\x0A\x0D\x20-\x7E]            # ASCII
-			| [\xC2-\xDF][\x80-\xBF]             # non-overlong 2-byte
-			| \xE0[\xA0-\xBF][\x80-\xBF]         # excluding overlongs
-			| [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}  # straight 3-byte
-			| \xED[\x80-\x9F][\x80-\xBF]         # excluding surrogates
-			| \xF0[\x90-\xBF][\x80-\xBF]{2}      # planes 1-3
-			| [\xF1-\xF3][\x80-\xBF]{3}          # planes 4-15
-			| \xF4[\x80-\x8F][\x80-\xBF]{2}      # plane 16
-			)*$%xs', $string)) {
+		    EncodeExplorer::getConfig('os_charset') != 'UTF-8' &&
+		    // From http://w3.org/International/questions/qa-forms-utf-8.html
+		    !preg_match('%^(?:
+		      [\x09\x0A\x0D\x20-\x7E]            # ASCII
+		    | [\xC2-\xDF][\x80-\xBF]             # non-overlong 2-byte
+		    | \xE0[\xA0-\xBF][\x80-\xBF]         # excluding overlongs
+		    | [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}  # straight 3-byte
+		    | \xED[\x80-\x9F][\x80-\xBF]         # excluding surrogates
+		    | \xF0[\x90-\xBF][\x80-\xBF]{2}      # planes 1-3
+		    | [\xF1-\xF3][\x80-\xBF]{3}          # planes 4-15
+		    | \xF4[\x80-\x8F][\x80-\xBF]{2}      # plane 16
+		    )*$%xs', $string)) {
 
 			// Attempt using mb_convert_encoding
 			if(function_exists('mb_convert_encoding')) {

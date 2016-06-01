3afa555801644361d09aaa55951b858e1d204645
Vicent Fernandez i Capilla <vfernandezc@llx-vfernandez.edu.gva.es>
2016-06-01 13:47:33 +0200

    Catalan translation added

--- index.php
+++ index.php
@@ -341,6 +341,25 @@ $_TRANSLATIONS["bg"] = array(
 	"del" => "Изтриване",
 	"log_out" => "Изход"
 );
+//Catalan
+$_TRANSLATIONS["ca"] = array(
+        "file_name" => "Nom d'arxiu",
+        "size" => "Mida",
+        "last_changed" => "Última modificació",
+        "total_used_space" => "Total espai emprat",
+        "free_space" => "Espai lliure",
+        "password" => "Paraula de pass",
+        "upload" => "Pujar arxiu",
+        "failed_upload" => "Error al pujar l'arxiu!",
+        "failed_move" => "Error al moure l'arxiu al directori seleccionat!",
+        "wrong_password" => "Paraula de pas incorrecta",
+        "make_directory" => "Crear directori",
+        "new_dir_failed" => "Error al crear el directori",
+        "chmod_dir_failed" => "Error al canviar els permisos del directori",
+        "unable_to_read_dir" => "No es possible llegir el directori",
+        "location" => "Traducció",
+        "root" => "Arrel"
+);
 
 // Czech
 $_TRANSLATIONS["cz"] = array(
